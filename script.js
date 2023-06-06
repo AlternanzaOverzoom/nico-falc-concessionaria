49470aa5a83c23ed54e3e0f9507179e6528fa482
Falce <falcettanico200@gmail.com>
2023-06-06 16:32:37 +0200

    aggiunti immagine presentazione e inizio sviluppo figurine

--- script.js
+++ script.js
@@ -21,16 +21,35 @@ function psw() {
         return true;
         
     }
+ }let slideIndex = 1;
+ showSlides(slideIndex);
+ 
+ // Next/previous controls
+ function plusSlides(n) {
+   showSlides((slideIndex += n));
  }
- function openNav() {
-  document.getElementById("side-nav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  document.getElementById("side-nav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-}
-  
-  
+ 
+ // Thumbnail image controls
+ function currentSlide(n) {
+   showSlides((slideIndex = n));
+ }
+ 
+ function showSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName("mySlides");
+   var dots = document.getElementsByClassName("dot");
+   if (n > slides.length) {
+     slideIndex = 1;
+   }
+   if (n < 1) {
+     slideIndex = slides.length;
+   }
+   for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+   }
+   for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex - 1].style.display = "block";
+   dots[slideIndex - 1].className += " active";
+ }
